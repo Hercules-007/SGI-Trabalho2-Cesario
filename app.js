bcf3037bc7fe3306dff3e0505fd89c64ba4365fd
Cesário <ccamposalves98@gmail.com>
2024-05-30 23:54:02 +0100

    export app
    Added app export for Vercel

--- app.js
+++ app.js
@@ -43,3 +43,5 @@ app.use('/items', require('./routes/item'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
