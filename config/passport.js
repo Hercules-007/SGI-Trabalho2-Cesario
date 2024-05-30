7816456fc9199e4a8373ffb3c1c9401b5a112f47
Cesário <ccamposalves98@gmail.com>
2024-05-31 00:58:03 +0100

    Update passport.js
    Changing callbackUEL to accept URL with app name

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  // callbackURL: '/auth/google/callback',
+  callbackURL: 'production' ? 'https://sgi-trabalho2-cesario-v2.vercel.app/auth/google/callback' : 'http://localhost:3000/auth/google/callback';
 }, async (accessToken, refreshToken, profile, done) => {
   const newUser = {
     googleId: profile.id,
