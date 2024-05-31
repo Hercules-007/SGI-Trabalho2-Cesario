e2ef12326e887d398a06d4a2a500851c9ba81d5e
Cesário <ccamposalves98@gmail.com>
2024-05-31 01:09:56 +0100

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback', 'https://sgi-trabalho2-cesario-v2.vercel.app/auth/google/callback'
+  callbackURL: 'https://sgi-trabalho2-cesario-v2.vercel.app/auth/google/callback'
+  // callbackURL: '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   const newUser = {
     googleId: profile.id,
